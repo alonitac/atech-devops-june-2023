29a56f4fe9b867a89c775ade0e87d0c688650e5d
kinanhino <kinanhino24@gmail.com>
2023-09-16 02:07:11 +0300

    bastion_connect.sh + vpc.sh ready

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
+if [ -z $KEY_PATH ]; then
+	echo "KEY_PATH env var is expected">&2
+	exit 5
+fi
 
-# TODO your solution here
+if [ "$#" -lt 1 ]; then
+	echo "Please prodive bastion IP address">&2
+	exit 5
+fi
+BASTION_IP=$1
+if [ "$#" -eq 1 ]; then
+	ssh -tt -i $KEY_PATH ubuntu@$BASTION_IP
+	echo "succesfully connected to bastion $BASTION_IP"
+	#exit 0
+elif [ "$#" -eq 2 ]; then
+	PRIVATE_IP=$2
+	ssh -i $KEY_PATH ubuntu@$BASTION_IP "./remote.sh $PRIVATE_IP"
+	#exit 0
+
+elif [ "$#" -gt 2 ]; then
+	PRIVATE_IP=$2
+	COMMAND=$3
+	ssh -i $KEY_PATH ubuntu@$BASTION_IP "./remote.sh $PRIVATE_IP '$COMMAND'"
+	#exit 0
+fi
+	
