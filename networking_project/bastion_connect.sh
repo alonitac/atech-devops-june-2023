e2cfeb810952f99d7b863b8ee79d4df1cce1947a
Nancy <nancyfanous1@gmail.com>
2023-09-18 21:49:25 +0300

    SSH Bastion Host and SSH Keys Rotation

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
 
+if [ -z "$KEY_PATH" ];
+then
+  echo "KEY_PATH env var is expected"
+  exit 5
+
+else
+  if [[ $# -eq 0 ]];
+  then
+     echo "provide IP adresses"
+     exit 5
+
+  elif [[ $# -eq 1 ]];
+  then
+    ssh -ti $KEY_PATH ubuntu@$1
+  elif [[ $# -eq 2 ]];
+  then
+    ssh -ti $KEY_PATH ubuntu@$1 ssh -ti test_key.pem ubuntu@$2
+  elif [[ $# -eq 3 ]];
+  then
+    ssh -i $KEY_PATH ubuntu@$1 ssh -i test_key.pem ubuntu@$2 $3
+  fi
+fi
+
 # TODO your solution here
