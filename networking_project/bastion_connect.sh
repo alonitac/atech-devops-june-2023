7c43aaa1e3d5906fc0df0f2d4a84320c4e9e6313
saeedwhamad <saeedhamad97@hotmail.com>
2023-09-23 17:17:56 +0300

    first solution

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
 
 
-# TODO your solution here
+
+if [[ -v $KEY_PATH ]]; then
+    ssh-agent bash
+    ssh-add $KEY_PATH
+else
+    echo "KEY_PATH env var is expected"
+    exit 5
+fi
+
+if [[ -v $1]]; then
+  ssh -A ubuntu@$1
+  else
+  echo "Please provide bastion IP address"
+  exit 5
+
+    fi
+
+
+
+
+if [[ -v $]]; then
+  ssh ubuntu@$2
+else
+
+
+fi
+
+if [[ -v $3]]; then
+
+eval "$3"
+   fi
