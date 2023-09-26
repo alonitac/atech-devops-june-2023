0a959ae5c1669e219811f743e504f8d003674fb1
kinanhino <kinanhino24@gmail.com>
2023-09-26 19:39:04 +0300

    fixing tlsHandshake issues

--- networking_project/tlsHandshake.sh
+++ networking_project/tlsHandshake.sh
@@ -11,19 +11,19 @@ SESSION_ID=$(echo $client_hello | jq -r '.sessionID')
 #server_cert=$(echo "$client_hello" | jq -r '.serverCert' | awk '{n = NF-1; for (i=1; i<=NF; i++) if (i == 3 || i == n-1) printf "%s\n", $i; else printf "%s ", $i}')
 server_cert1=$(echo "$client_hello" | jq -r '.serverCert') #| sed 's/ /\n/2')
 #server_cert=$(echo "$server_cert1" | sed 's/ \(.*\) /\1\n/')
-echo "$server_cert1" > server_certification.pem
+echo "$server_cert1" > server_cert.pem
 #echo $server_cert
 #server_cert=$(echo $client_hello | awk '{print $8,$9.$10,$11,$12}' | sed -e '1s/^.//' | sed 's/.\{4\}$//')
 #echo $server_cert > server_cert1.pem
 
-openssl verify -CAfile cert-ca-aws.pem server_certification.pem 
+openssl verify -CAfile cert-ca-aws.pem server_cert.pem
 if [ "$?" -ne 0 ]; then 
 	echo "Server Certificate is invalid.">&2
 	exit 5
 fi
 openssl rand -base64 32 > master_key.txt
 
-MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in master_key.txt -outform DER server_certification.pem | base64 -w 0)
+MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in master_key.txt -outform DER server_cert.pem | base64 -w 0)
 #MASTER_ID=$(cat encrypted_master.txt)
 echo $MASTER_KEY > encoded_master.txt
 server_encrypted=$(curl -X POST -H "Content-Type: application/json" -d '{"sessionID": "'$SESSION_ID'", "masterKey": "'$MASTER_KEY'", "sampleMessage": "Hi server, please encrypt and send to client!"}' 18.193.127.38:8080/keyexchange)
