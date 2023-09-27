513c745750f0951418414e38a77c9710e302c81c
saraa <saraa.elhamid97@gmail.com>
2023-09-28 02:06:03 +0300

    test_tlsHandshake

--- networking_project/tlsHandshake.sh
+++ networking_project/tlsHandshake.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 RESPONSE=$(curl -X POST -H "Content-Type: application/json" -d '{"version": "1.3","ciphersSuites": ["TLS_AES_128_GCM_SHA256","TLS_CHACHA20_POLY1305_SHA256" ], "message": "Client Hello"}' 18.226.201.60:8080/clienthello)
 
 SESSION_ID=$(echo $RESPONSE | jq -r '.sessionID')
