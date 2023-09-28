f3bcda7124e5fff5bc6bbf48ca747629cafa50c8
saraa <saraa.elhamid97@gmail.com>
2023-09-28 18:34:15 +0300

    test_new_branch

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,4 +1,26 @@
-#!/bin/bash
+!/bin/bash
 
+#bad usage
+if [ ! $KEY_PATH ]
+then 
+        echo "KEY_PATH env var is expected"
+        exit 5
+fi
 
-# TODO your solution here
+#bad usage
+if [ $# -lt 1 ]
+then 
+        echo "Please provide bastion IP address"
+        exit 5
+#connect to public instance
+elif [ $# -eq 1 ]
+then 
+        ssh -i $KEY_PATH ubuntu@$1
+#connect to private instance from local machine
+elif [ $# -eq 2 ]
+then 
+        ssh -i $KEY_PATH ubuntu@$1 "./ssh_private_instance.sh $2"
+#run a command in the private machine
+else 
+        ssh -i $KEY_PATH ubuntu@$1 "./ssh_private_instance.sh $2 ${@:3}"
+fi
--- networking_project/tlsHandshake.sh
+++ networking_project/tlsHandshake.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
 
-# TODO Your solution here
+RESPONSE=$(curl -X POST -H "Content-Type: application/json" -d '{"version": "1.3","ciphersSuites": ["TLS_AES_128_GCM_SHA256","TLS_CHACHA20_POLY1305_SHA256" ], "message": "Client Hello"}' 3.12.146.148:8080/clienthello)
+
+SESSION_ID=$(echo $RESPONSE | jq -r '.sessionID')
+
+echo $RESPONSE | jq -r '.serverCert' > cert.pem
+
+openssl verify -CAfile cert-ca-aws.pem cert.pem
+
+if [ $? -ne 0 ]
+then
+	echo "Server Certificate is invalid."
+	exit 5
+fi
+
+openssl rand -base64 32 > masterKey
+
+MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in masterKey -outform DER cert.pem | base64 -w 0)
+
+KEY_RESPONSE=$(curl -X POST -H "Content-Type: application/json" -d '{"sessionID": "'$SESSION_ID'","masterKey": "'$MASTER_KEY'","sampleMessage": "Hi server, please encrypt me and send to client!"}' 3.12.146.148:8080/keyexchange)
+
+echo $KEY_RESPONSE | jq -r '.encryptedSampleMessage' | base64 -d > encSampleMsgReady.txt
+
+echo "Hi server, please encrypt me and send to client!" > sampleMessage.txt
+
+openssl enc -d -aes-256-cbc -in encSampleMsgReady.txt -out decryptedMsg.txt -pbkdf2 -kfile masterKey
+
+diff sampleMessage.txt decryptedMsg.txt
+
+if [ $? -ne 0 ]
+then
+	echo "Server symmetric encryption using the exchanged master-key has failed."
+	exit 6
+else
+	echo "Client-Server TLS handshake has been completed successfully"
+fi
