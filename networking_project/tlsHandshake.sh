09ac8c352d1f7345953385f52f4a6daf3c395790
mohamedabubaker09 <mohamedabubaker09@gmail.com>
2023-09-28 17:06:25 +0300

    fix tls 10

--- networking_project/tlsHandshake.sh
+++ networking_project/tlsHandshake.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-client_hello=$(curl -X POST -H "Content-Type: application/json" -d '{"version": "1.3", "ciphersSuites": ["TLS_AES_128_GCM_SHA256", "TLS_CHACHA20_POLY1305_SHA256"], "message": "Client Hello" }'  13.53.186.104:8080/clienthello)
-echo $client_hello
-exit
+client_hello=$(curl -X POST -H "Content-Type: application/json" -d '{"version": "1.3", "ciphersSuites": ["TLS_AES_128_GCM_SHA256", "TLS_CHACHA20_POLY1305_SHA256"], "message": "Client Hello" }'  	13.48.23.235:8080/clienthello)
+#echo $client_hello
+#exit
 
 SESSION_ID=$(echo $client_hello | jq -r '.sessionID')
 server_cert1=$(echo "$client_hello" | jq -r '.serverCert')
@@ -18,12 +18,9 @@ fi
 openssl rand -base64 32 > master_key.txt
 MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in master_key.txt -outform DER server_cert.pem | base64 -w 0)
 echo $MASTER_KEY > encoded_master.txt
-server_encrypted=$(curl -X POST -H "Content-Type: application/json" -d '{"sessionID": "'$SESSION_ID'", "masterKey": "'$MASTER_KEY'", "sampleMessage": "Hi server, please encrypt and send to client!"}' 13.53.186.104:8080/keyexchange)
+server_encrypted=$(curl -X POST -H "Content-Type: application/json" -d '{"sessionID": "'$SESSION_ID'", "masterKey": "'$MASTER_KEY'", "sampleMessage": "Hi server, please encrypt and send to client!"}' 	13.48.23.235:8080/keyexchange)
 
 encrypted_message=$(echo $server_encrypted | awk '{print $4}' | sed 's/.\{2\}$//' | sed -e '1s/^.//')
-#echo ""
-#echo ""
-#echo ""
 
 #encoded in base 64
 echo $encrypted_message | base64 -d > encoded.txt
