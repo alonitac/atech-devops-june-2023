7dff322301eb22cd7da407357e4ff3690b3d7af3
MaramAlasad <maramalasad9@gmail.com>
2023-09-29 08:11:54 +0300

    my solution

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+if [[ -z $KEY_PATH ]] ; then
+  echo 'KEY_PATH env var is expected'
+  exit 2
+fi
+CMD="ssh -i $KEY_PATH"
+if [[ -z $1 ]] ; then
+  echo 'Please provide bastion IP address'
+  exit 2
+fi
+if [[ -n $3 ]] ; then
+  CMD="$CMD -o ProxyCommand=\"ssh -W %h:%p -i $KEY_PATH ubuntu@$1\"  ubuntu@$2 $3"
+  else
+    CMD="$CMD ubuntu@$1 $2"
+fi
+eval $CMD
 
-
-# TODO your solution here
--- networking_project/tlsHandshake.sh
+++ networking_project/tlsHandshake.sh
@@ -1,3 +1,15 @@
-#!/bin/bash
-
-# TODO Your solution here
+##!/bin/bash
+#exit_state(){
+#  local var1=$1
+#  if [[ "$var1" == 0 ]] ; then
+#    echo "exite succ"
+#  fi
+#}
+#url="ec2-54-78-48-90.eu-west-1.compute.amazonaws.com:8080/clienthello"
+#details='{"version": "1.3", "ciphersSuites": ["TLS_AES_128_GCM_SHA256", "TLS_CHACHA20_POLY1305_SHA256"], "message": "Client Hello" }'
+#client_hello="$( curl -X POST -H "Content-Type: application/json" -d "$details"  "$url" )" ; exit_state "$?"
+#SESSION_ID=$(echo $client_hello | jq -r '.sessionID')
+#server_cert1=$(echo "$client_hello" | jq -r '.serverCert')
+#echo $server_cert1
+#
+#
