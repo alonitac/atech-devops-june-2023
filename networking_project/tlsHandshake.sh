e30bc396c89cebc25c0f14a7383e2e93c5cd4050
ShirinLabay <labay.shirin@gmail.com>
2023-09-29 20:53:07 +0300

    test

--- networking_project/tlsHandshake.sh
+++ networking_project/tlsHandshake.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-clienthello=$(curl -X POST -H "Content-Type: application/json" -d '{"version": "1.3","ciphersSuites": ["TLS_AES_128_GCM_SHA256","TLS_CHACHA20_POLY1305_SHA256" ], "message": "Client Hello"}' 18.194.208.240:8080/clienthello)
+clienthello=$(curl -X POST -H "Content-Type: application/json" -d '{"version": "1.3","ciphersSuites": ["TLS_AES_128_GCM_SHA256","TLS_CHACHA20_POLY1305_SHA256" ], "message": "Client Hello"}' 3.73.119.203:8080/clienthello)
 #echo $clienthello
 
 SESSION_ID=$(echo $clienthello | jq -r '.sessionID')
 #echo $SESSION_ID
 
 #save cert
-certificate=$(echo $clienthello | jq -r '.serverCert')
-#echo $certificate
-echo "$certificate" > cert.pem
+echo "$clienthello" | jq -r '.serverCert' > cert.pem
+
 
 openssl verify -CAfile cert-ca-aws.pem cert.pem
 if [ $? -ne 0 ]; then
@@ -23,7 +22,7 @@ openssl rand -base64 32 > master_key
 #encrypt the master_key with the server certificate
 MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in master_key -outform DER cert.pem | base64 -w 0)
 
-enc_key_req=$(curl -X POST -H "Content-Type: application/json" -d '{"sessionID": "'$SESSION_ID'","masterKey": "'$MASTER_KEY'","sampleMessage": "Hi server, Please encrypt me and send to client!"}' 18.194.208.240:8080/keyexchange)
+enc_key_req=$(curl -X POST -H "Content-Type: application/json" -d '{"sessionID": "'$SESSION_ID'","masterKey": "'$MASTER_KEY'","sampleMessage": "Hi server, Please encrypt me and send to client!"}' 3.73.119.203:8080/keyexchange)
 
 msg=$(echo $enc_key_req | jq -r '.encryptedSampleMessage')
 echo $msg | base64 -d > enc_msg.txt
