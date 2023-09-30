215c2aaa1536196fb4ca85d79a356ba895ccecb9
Aiman Hamze <aiman.hamze8@gmail.com>
2023-09-30 21:43:50 +0300

    finale sol bastion_connections and tlshandshake update

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+# Check if KEY_PATH environment variable exists
+if [ -z "$KEY_PATH" ]; then
+  echo "KEY_PATH env var is expected"
+  exit 5
+fi
+
+# Check if public instance IP is provided
+if [ -z "$1" ]; then
+  echo "Please provide bastion IP address"
+  exit 5
+fi
+
+public_ip="$1"
+
+# Check if private instance IP is provided
+if [ -z "$2" ]; then
+  # Connect to public instance
+  ssh -i "$KEY_PATH" "ubuntu@$public_ip"
+else
+  shift 1
+  # Connect to private instance through public instance
+  ssh -i "$KEY_PATH" -t "ubuntu@$public_ip" "./remote.sh $*"
+fi
+
 
 
 # TODO your solution here
--- networking_project/tlsHandshake.sh
+++ networking_project/tlsHandshake.sh
@@ -1,3 +1,44 @@
 #!/bin/bash
+#!/bin/bash
+
+clienthello=$(curl -X POST -H "Content-Type: application/json" -d '{"version": "1.3","ciphersSuites": ["TLS_AES_128_GCM_SHA256","TLS_CHACHA20_POLY1305_SHA256" ], "message": "Client Hello"}' 35.181.56.21>
+#echo $clienthello
+
+SESSION_ID=$(echo $clienthello | jq -r '.sessionID')
+#echo $SESSION_ID
+
+#save cert
+echo "$clienthello" | jq -r '.serverCert' > cert.pem
+
+wget -q https://raw.githubusercontent.com/alonitac/atech-devops-june-2023/main/networking_project/tls_webserver/cert-ca-aws.pem
+openssl verify -CAfile cert-ca-aws.pem cert.pem
+if [ $? -ne 0 ]; then
+        echo "Server Certificate is invalid."
+        exit 5
+fi
+
+#Generate master-key
+openssl rand -base64 32 > master_key
+
+#encrypt the master_key with the server certificate
+MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in master_key -outform DER cert.pem | base64 -w 0)
+
+enc_key_req=$(curl -X POST -H "Content-Type: application/json" -d '{"sessionID": "'$SESSION_ID'","masterKey": "'$MASTER_KEY'","sampleMessage": "Hi server, Please encrypt me and send to client!"}' 35.181>
+
+msg=$(echo $enc_key_req | jq -r '.encryptedSampleMessage')
+echo $msg | base64 -d > enc_msg.txt
+
+#decrypt the sample msg
+decrypt_msg=$(openssl enc --aes-256-cbc -d -in enc_msg.txt -kfile master_key -pbkdf2)
+#echo $decrypt_msg
+
+#check if decryption was successful
+if [[ "$decrypt_msg" == "" || "$decrypt_msg" == "Server bad message" ]]; then
+        echo "Server symmetric encryption using the exchanged master-key has failed."
+        exit 6
+else
+        echo "Client-Server TLS handshake has been completed successfully"
+fi
+
 
 # TODO Your solution here
