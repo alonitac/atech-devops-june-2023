fa814d1ec30535b533d829cae2efd8f3df529730
saeedwhamad <saeedhamad97@hotmail.com>
2023-10-02 00:04:56 +0300

    tls handshakcae edit 26

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
 eval "$(ssh-agent -s)"
 
 
 if [[ -n $KEY_PATH ]]; then
   ssh-add $KEY_PATH
-  ssh -A  ubuntu@$1 << EOF
-  ssh ubuntu@$2
-EOF
+  ssh -A  ubuntu@$1 -t ssh ubuntu@$2
 
 fi
