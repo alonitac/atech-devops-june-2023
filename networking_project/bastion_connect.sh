547dd90de10bb543e52525b327d3064308e7f150
saeedwhamad <saeedhamad97@hotmail.com>
2023-10-02 02:51:20 +0300

    2

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cat $KEY_PATH > ~/priKEY
-chmod 600 ~/priKEY
-scp ~/priKEY ubuntu@$1:~/
-ssh -i $KEY_PATH ubuntu@$1 "ssh -i ~/priKEY ubuntu@$2 "
+
+ssh-copy-id -i $KEY_PATH ubuntu@$1:~/
+ssh -i $KEY_PATH ubuntu@$1 "ssh -i ~/ ubuntu@$2 "
