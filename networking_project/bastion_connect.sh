6e20c1689005d9ebac4d409fad504337e56ca751
saeedwhamad <saeedhamad97@hotmail.com>
2023-10-01 22:05:51 +0300

    tls handshakcae edit 11

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/ssh-agent bash
 
-chmod 600 KEY_PATH
 
 
-if [[ -v KEY_PATH ]]; then
 
-
- if [[ -z $1 ]]; then
-echo "Please provide bastion IP address"
-     exit 5
-
-else
-if [[ -z $2 ]]; then
-    ssh-add $KEY_PATH
-    ssh -A ubuntu@$1
-
-else
+if [[ -v $KEY_PATH ]]; then
+  if [[ -z $1 ]]; then
+      echo "Please provide bastion IP address"
+      exit 5
+  else
+    if [[ -z $2 ]]; then
+      ssh-add $KEY_PATH
+      ssh -A ubuntu@$1
+      else
  if [[ -z $3 ]]; then
 ssh-add $KEY_PATH
  ssh -A ubuntu@$1 -t ssh ubuntu@$2
