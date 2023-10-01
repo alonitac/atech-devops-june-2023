0975ea2ede7046e467007ff3470725ae21936e33
saeedwhamad <saeedhamad97@hotmail.com>
2023-10-01 23:29:59 +0300

    tls handshakcae edit 16

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,31 +1,8 @@
-#!/bin/bash
+#!/usr/bin/ssh-agent bash
 
 
 if [[ -n $KEY_PATH ]]; then
-
-  if [[ -z $1 ]]; then
-      echo "Please provide bastion IP address"
-      exit 5
-  else
-    if [[ -z $2 ]]; then
-
-      ssh -i $KEY_PATH ubuntu@$1
-      else
- if [[ -z $3 ]]; then
-
- ssh -A ubuntu@$1 -t ssh ubuntu@$2
-else
-
-               ssh -A ubuntu@$1 -t ssh ubuntu@$2 -t eval $3
-           fi
-
-     fi
-
-
-
- fi
-else
- echo "KEY_PATH env var is expected"
-  exit 5
-
-fi
+  ssh-add $KEY_PATH
+  ssh -A  ubuntu@$1 << EOF
+   ssh ubuntu@$2
+   fi
