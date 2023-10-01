b5564ef46fb61ed50f2bec8360445c848538086a
Anas Halloumi <57338202+anasha-dot@users.noreply.github.com>
2023-10-01 19:45:05 +0300

    b

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -3,21 +3,21 @@
 
 # TODO your solution here
 
-if [[ -z "${KEY_PATH}" ]]; then
+if [[-z "${KEY_PATH}" ]]; then
     echo "KEY_PATH env var is expected"
     exit 5 
 else
-    if [ $# -eq 0 ]
+    if ["$#" -eq 0]
     then 
         echo "Please provide bastion IP address"
         exit 5
-    elif [ $# -eq 1 ]
+    elif ["$#" -eq 1]
     then
         ssh -i $KEY_PATH ubuntu@$1
-    elif [ $# -eq 2 ]
+    elif ["$#" -eq 2]
     then
         ssh -tt -i $KEY_PATH ubuntu@$1 ssh -i $KEY_PATH ubuntu@$2
-    elif [ $# -gt 2 ]
+    elif ["$#" -gt 2]
     then
         ssh -tt -i $KEY_PATH ubuntu@$1 ssh -i $KEY_PATH ubuntu@$2 ${@:3}
     fi
