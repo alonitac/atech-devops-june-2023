c19abfd56b89d3de138e956109c865ff9fa1e606
nasimob <nasim.obaid12@gmail.com>
2023-10-01 06:38:03 +0300

    networking project (tlsHandshake fix 01)

--- networking_project/tlsHandshake.sh
+++ networking_project/tlsHandshake.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-if [ $# -lt 1 ]
-then
-  echo "Enter an IP address"
-  exit 5
-fi
 
-IP=$1
-export Client_hello=$(curl -X POST -H "Content-Type: application/json" -d '{"version": "1.3", "ciphersSuites": ["TLS_AES_128_GCM_SHA256", "TLS_CHACHA20_POLY1305_SHA256"], "message": "Client Hello" }'  ${IP}:8080/clienthello)
+Client_hello=$(curl -X POST -H "Content-Type: application/json" -d '{"version": "1.3", "ciphersSuites": ["TLS_AES_128_GCM_SHA256", "TLS_CHACHA20_POLY1305_SHA256"], "message": "Client Hello" }'  51.20.76.73:8080/clienthello)
 SessionID=$(echo $Client_hello | jq -r '.sessionID')
 ServerCert=$(echo $Client_hello | jq -r '.serverCert')
 
 cat <<EOF > ServerCert.pem
 $ServerCert
 EOF
+wget https://raw.githubusercontent.com/alonitac/atech-devops-june-2023/main/networking_project/tls_webserver/cert-ca-aws.pem
 
 if ! openssl verify -CAfile cert-ca-aws.pem ServerCert.pem
 then
@@ -28,7 +23,7 @@ cat <<EOF > encoded_master_key.txt
 $MASTER_KEY
 EOF
 
-export server_encrypted=$(curl -X POST -H "Content-Type: application/json" -d '{"sessionID": "'$SessionID'", "masterKey": "'$MASTER_KEY'", "sampleMessage": "Hi server, please encrypt and send to client!"}' ${IP}:8080/keyexchange)
+export server_encrypted=$(curl -X POST -H "Content-Type: application/json" -d '{"sessionID": "'$SessionID'", "masterKey": "'$MASTER_KEY'", "sampleMessage": "Hi server, please encrypt and send to client!"}' 51.20.76.73:8080/keyexchange)
 encrypted_sample_msg=$(echo $server_encrypted | jq -r '.encryptedSampleMessage') 
 echo $encrypted_sample_msg | base64 -d > encSampleMsgReady.txt
 
