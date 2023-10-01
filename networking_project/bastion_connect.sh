c6f7c108b5156b1c25dd679f7568345b185bfb5c
saeedwhamad <saeedhamad97@hotmail.com>
2023-10-01 23:09:21 +0300

    tls handshakcae edit 14

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -2,19 +2,20 @@
 
 
 if [[ -n $KEY_PATH ]]; then
+  ssh-add $KEY_PATH
   if [[ -z $1 ]]; then
       echo "Please provide bastion IP address"
       exit 5
   else
     if [[ -z $2 ]]; then
-      ssh-add $KEY_PATH
-      ssh -A ubuntu@$1
+
+      ssh ubuntu@$1
       else
  if [[ -z $3 ]]; then
-ssh-add $KEY_PATH
+
  ssh -A ubuntu@$1 -t ssh ubuntu@$2
 else
-ssh-add $KEY_PATH
+
                ssh -A ubuntu@$1 -t ssh ubuntu@$2 -t eval $3
            fi
 
