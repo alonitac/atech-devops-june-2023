113ebf5fe0d5fd53261f5914c1cdbf6625300ded
Anas Halloumi <57338202+anasha-dot@users.noreply.github.com>
2023-10-01 21:25:48 +0300

    ps

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -4,6 +4,9 @@
 # TODO your solution here
 #!/bin/bash
 
+#!/bin/bash
+publicip=$1
+privateip=$2
 if [ -z "${KEY_PATH}" ]; then
         echo "KEY_PATH env var is expected"
         exit 5 
@@ -14,17 +17,13 @@ else
                 exit 5
         elif [ "$#" -eq 1 ]
         then
-                ssh -i "$KEY_PATH" ubuntu@"$1"
-        elif [ "$#" -eq 2 ]
-        then
-                ssh -i "$KEY_PATH" ubuntu@"$1" "ssh -i $KEY_PATH ubuntu@$2"
-        elif [ "$#" -gt 2 ]
-        then    
+                ssh -i "$KEY_PATH" ubuntu@"$publicip"
+        
+        else
                 cmd=${@:3}
-                ssh -i "$KEY_PATH" -t "ubuntu@$1" "ssh -i $KEY_PATH ubuntu@$2 '$cmd'"
+                ssh -i "$KEY_PATH" -t "ubuntu@$1" "./privatecmd.sh '$privateip $cmd'"
         fi
 fi
-
 
 # if [[-z "${KEY_PATH}" ]]; then
 #     echo "KEY_PATH env var is expected"
