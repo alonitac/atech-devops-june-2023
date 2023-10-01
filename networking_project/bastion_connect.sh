b5842cab27313275e986b77e92d817049a31e4fc
Anas Halloumi <57338202+anasha-dot@users.noreply.github.com>
2023-10-01 20:59:14 +0300

    b

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -2,30 +2,29 @@
 
 
 # TODO your solution here
-#bad usage
-if [ ! $KEY_PATH ]
-then
+#!/bin/bash
+
+if [ -z "${KEY_PATH}" ]; then
         echo "KEY_PATH env var is expected"
-        exit 5
+        exit 5 
+else
+        if [ "$#" -eq 0 ]
+        then 
+                echo "Please provide bastion IP address"
+                exit 5
+        elif [ "$#" -eq 1 ]
+        then
+                ssh -i "$KEY_PATH" ubuntu@"$1"
+        elif [ "$#" -eq 2 ]
+        then
+                ssh -i "$KEY_PATH" ubuntu@"$1" "ssh -i $KEY_PATH ubuntu@$2"
+        elif [ "$#" -gt 2 ]
+        then    
+                cmd=${@:3}
+                ssh -i "$KEY_PATH" -t "ubuntu@$1" "ssh -i $KEY_PATH ubuntu@$2 '$cmd'"
+        fi
 fi
 
-#bad usage
-if [ $# -lt 1 ]
-then
-        echo "Please provide bastion IP address"
-        exit 5
-#connect to public instance
-elif [ $# -eq 1 ]
-then
-        ssh -i $KEY_PATH ubuntu@$1
-#connect to private instance from local machine
-elif [ $# -eq 2 ]
-then
-        ssh -i $KEY_PATH ubuntu@$1 "ssh -i $KEY_PATH ubuntu@$2"
-#run a command in the private machine
-else
-        ssh -i $KEY_PATH ubuntu@$1 "ssh -i $KEY_PATH ubuntu@$2 ${@:3}"
-fi
 
 # if [[-z "${KEY_PATH}" ]]; then
 #     echo "KEY_PATH env var is expected"
