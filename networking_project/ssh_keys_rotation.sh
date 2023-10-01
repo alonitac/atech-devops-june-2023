6bbc99a405088ba5375aa45e81e926d5fdef7ec9
saeedwhamad <saeedhamad97@hotmail.com>
2023-10-02 01:38:56 +0300

    4

--- networking_project/ssh_keys_rotation.sh
+++ networking_project/ssh_keys_rotation.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/ssh-agent bash
 
-ssh-add $KEY_PATH
-ssh -A ubuntu@$1 -t ubuntu@$2
+if[[ -n $KEY_PATH]];then
+ ssh-add $KEY_PATH
+ if[[ -z $1]];then
+  else
+    if[[ -z $2]];then
+       ssh ubuntu@1
+       else
+        if[[ -z $3]];then
+          ssh -A ubuntu@$1 -t ubuntu@$2
+        else
+          ssh -A ubuntu@$1 -t ubuntu@$2 -t eval $3
+        fi
+    fi
+ fi
+fi
