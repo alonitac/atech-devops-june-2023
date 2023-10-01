ddd1558f9ffed71d27c566a72c594ccba963738a
saeedwhamad <saeedhamad97@hotmail.com>
2023-10-02 00:18:03 +0300

    tls handshakcae edit 29

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -5,6 +5,6 @@ eval "$(ssh-agent -s)"
 
 if [[ -n $KEY_PATH ]]; then
   ssh-add $KEY_PATH
-  ssh -A  ubuntu@$1 "whoami; ls -l ~/.ssh/"
+  ssh -A  ubuntu@$1 -t ssh -o "StrictHostKeyChecking no" ubuntu@$2
 
 fi
