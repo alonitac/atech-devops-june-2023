2ae1d7837ec4b2141363fe86f7ea3be94d224563
saeedwhamad <saeedhamad97@hotmail.com>
2023-10-02 22:11:39 +0300

    bastion repair

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
 
-ssh-copy-id -i $KEY_PATH ubuntu@$1:~/
-ssh -i $KEY_PATH ubuntu@$1 "ssh -i ~/ ubuntu@$2 "
+if [[ -n $KEY_PATH ]]; then
+
+  if [[ -z $1 ]]; then
+      echo "Please provide bastion IP address"
+      exit 5
+  else
+    if [[ -z $2 ]]; then
+
+      ssh -i $KEY_PATH ubuntu@$1
+      else
+ if [[ -z $3 ]]; then
+
+ ssh -A ubuntu@$1 -t "bash ./connectPrivate"
+else
+
+               ssh -A ubuntu@$1 -t "bash ./connectPrivate&run "ls""
+           fi
+
+     fi
+
+
+
+ fi
+else
+ echo "KEY_PATH env var is expected"
+  exit 5
+
+fi
