7107b8f2ce6dfc09930337b0cd0dca75b40080dc
saeedwhamad <saeedhamad97@hotmail.com>
2023-10-02 22:23:02 +0300

    bastion repair

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+if[[ -n $KEY_PATH ]];then
 
 
+ssh -i $KEY_PATH ubuntu@$1
 
-      ssh -i $KEY_PATH ubuntu@$1
 
-
- ssh -i $KEY_PATH ubuntu@$1 -t "bash ./connectPrivate"
+ssh -i $KEY_PATH ubuntu@$1 -t "bash ./connectPrivate"
+else
+  echo "f"
+  exit 5
+  fi
