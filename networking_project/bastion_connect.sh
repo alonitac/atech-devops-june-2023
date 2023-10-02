09a7a3d6e2434e68a577d068bb4d90cff31a6bab
saeedwhamad <saeedhamad97@hotmail.com>
2023-10-02 22:52:49 +0300

    bastion repair

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -11,7 +11,7 @@ if [[ -n $KEY_PATH ]]; then
       if [[ -z $3 ]]; then
         ssh -i $KEY_PATH ubuntu@$1 -t "bash ./connectPrivate"
       else
-        echo "hi"
+        ssh -i $KEY_PATH ubuntu@$1 -t "bash ./connectPrivateRun $3"
       fi
     fi
   fi
