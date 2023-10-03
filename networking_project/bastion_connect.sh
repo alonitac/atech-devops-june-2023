643cc520e30d8e1d3c99f8f2d9b132be2d4d7038
saeedwhamad <saeedhamad97@hotmail.com>
2023-10-03 19:52:43 +0300

    final submission

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+#check if there is a key path
 if [[ -n $KEY_PATH ]]; then
+#check if there is a public ip
   if [[ -z $1 ]]; then
     echo "Please provide bastion IP address"
     exit 5
   else
+#check if there is public ip and private ip or only private
     if [[ -z $2 ]]; then
       ssh -i $KEY_PATH ubuntu@$1
     else
+#check if there is a command to run on private machine
       if [[ -z $3 ]]; then
         ssh -i $KEY_PATH ubuntu@$1 -t "ssh -i ~/new_key ubuntu@$2"
       else
-        ssh -i $KEY_PATH ubuntu@$1 -t "ssh -i ~/new_key ubuntu@10.0.1.192 -t  "${@:3}""
+        ssh -i $KEY_PATH ubuntu@$1 -t "ssh -i ~/new_key ubuntu@$2 -t  "${@:3}""
       fi
     fi
   fi
