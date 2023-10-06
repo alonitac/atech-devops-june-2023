3dec80a91002ea21cecc9b0f7a7735691211a001
marwazu <marwa.zu7@gmail.com>
2023-10-06 03:26:15 +0300

    try1

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
+#check if the variable KEY_PATH exists
+if [ -z "${KEY_PATH}" ]; then
+  echo "KEY_PATH env variable is expected"
+  exit 5
+fi
 
-# TODO your solution here
+# if no IP address was provided
+if [ $# -eq 0 ]; then
+  echo "Please provide bastion IP address"
+  exit 5
+#connect to the public instance1
+elif [ $# -eq 1 ]; then
+  PUBLIC_IP=$1
+  ssh -i $KEY_PATH ubuntu@$PUBLIC_IP
+# connect to the private instance from your local machine
+elif [ $# -eq 2 ]; then
+ PUBLIC_IP=$1
+ PRIVATE_IP=$2
+  ssh -i $KEY_PATH ubuntu@$PUBLIC_IP "./run_private_instance.sh $PRIVATE_IP"
+# run a command in the private machine
+elif [ $# -gt 2 ];then
+  PUBLIC_IP=$1
+  PRIVATE_IP=$2
+  ssh -i $KEY_PATH ubuntu@$PUBLIC_IP "./run_private_instance.sh $PRIVATE_IP ${@:3}"
+fi
