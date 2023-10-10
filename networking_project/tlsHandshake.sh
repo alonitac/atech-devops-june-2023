32bfcb5e94d088305367a3df362c93de78d651ed
marwazu <marwa.zu7@gmail.com>
2023-10-11 02:23:07 +0300

    11

--- networking_project/tlsHandshake.sh
+++ networking_project/tlsHandshake.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
+clientHello=$(curl -X POST -H "Content-Type: application/json" -d '{"version": "1.3","ciphersSuites": ["TLS_AES_128_GCM_SHA256","TLS_CHACHA20_POLY1305_SHA256" ], "message": "Client Hello"}' 3.145.80.154:8080/clienthello)
 
-# TODO Your solution here
+SESSION_ID=$(echo $clientHello | jq -r '.sessionID')
+
+echo "$clientHello" | jq -r '.serverCert' > cert.pem
+
+wget https://raw.githubusercontent.com/alonitac/atech-devops-june-2023/main/networking_project/tls_webserver/cert-ca-aws.pem
+openssl verify -CAfile cert-ca-aws.pem cert.pem
+
+if [ "$?"-ne 0 ]; then
+  echo "Server Certificate is invalid."
+  exit 5
+fi
+
+# generate the master key
+openssl rand -base64 32 > master_key
+
+masterKey=$(openssl smime -encrypt -aes-256-cbc -in master_key -outform DER cert.pem | base64 -w 0)
+
+
+responseKey=$(curl -X POST -H "Content-Type: application/json" -d '{"sessionID": "'$SESSION_ID'","masterKey": "'$masterKey'","sampleMessage": "Hi server, please encrypt me and send to client!"}' 3.145.80.154:8080/keyexchange)
+
+msg=$(echo "$responseKey" | jq -r '.encryptedSampleMessage')
+
+echo "$msg" | base64 -d > enc_massage
+
+echo $responseKey | jq -r '.encryptedSampleMessage' | base64 -d > enc_message
+
+# Decrypt
+decrypted=$(openssl enc -aes-256-cbc -d -in encSample.txt -pbkdf2 -kfile master_key)
+
+## check
+
+if [ $decrypted=="" || $decrypted=="Server bad message"]
+then
+  echo "Server symmetric encryption using the exchanged master-key has failed."
+  exit 6
+else
+  echo "Client-Server TLS handshake has been completed successfully"
+fi
