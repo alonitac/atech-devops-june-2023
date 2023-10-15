96eb11a4384e1d0478df79892310185063eb35f3
joumana <joumana.1444@gmail.com>
2023-10-15 18:17:01 +0300

    solution1

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,4 +1,47 @@
 #!/bin/bash
 
+# Check if KEY_PATH environment variable exists
 
-# TODO your solution here
+if [ -z "$KEY_PATH" ]; then
+  echo "KEY_PATH env var is expected"
+  exit 5
+fi
+
+# Check if the script has at least one argument
+if [ $# -lt 1 ]; then
+  echo "Please provide bastion IP address"
+  exit 5
+fi
+
+# Set the key file path based on the KEY_PATH environment variable
+KEY_FILE="$KEY_PATH"
+
+# Extract the bastion IP address
+BASTION_IP="$1"
+shift
+
+# If there's another argument, it's the private instance IP
+if [ $# -gt 0 ]; then
+  PRIVATE_IP="$1"
+  shift
+
+  # Check if there are more arguments (command to run on the private machine)
+  if [ $# -gt 0 ]; then
+    COMMAND="$@"
+  fi
+fi
+
+# Construct the SSH command depending on whether a private IP is provided
+if [ -n "$PRIVATE_IP" ]; then
+  if [ ! -n "$PRIVATE_KEY_PATH" ]; then
+    PRIVATE_KEY_PATH="$KEY_PATH"
+  fi
+
+  if [ -n "$COMMAND" ]; then
+    ssh -i "$KEY_FILE" -t -A "ubuntu@$BASTION_IP" "ssh -i new_key -A 'ubuntu@$PRIVATE_IP' \"$COMMAND\""
+  else
+    ssh -i "$KEY_FILE" -t -A "ubuntu@$BASTION_IP" "ssh -i new_key -A 'ubuntu@$PRIVATE_IP'"
+  fi
+else
+  ssh -i "$KEY_FILE" "ubuntu@$BASTION_IP" "$@"
+fi
--- networking_project/tlsHandshake.sh
+++ networking_project/tlsHandshake.sh
@@ -1,3 +1,50 @@
 #!/bin/bash
+# Step 1 - Client Hello
+CLIENT_HELLO='{
+   "version": "1.3",
+   "ciphersSuites": [
+      "TLS_AES_128_GCM_SHA256",
+      "TLS_CHACHA20_POLY1305_SHA256"
+    ],
+   "message": "Client Hello"
+}'
 
-# TODO Your solution here
+SERVER_RESPONSE=$(curl -X POST http://$PUBLIC_IP:8080/clienthello -H "Content-Type: application/json" -d "$CLIENT_HELLO")
+echo $SERVER_RESPONSE
+SESSION_ID=$(echo "$SERVER_RESPONSE" | jq -r '.sessionID')
+SERVER_CERT=$(echo "$SERVER_RESPONSE" | jq -r '.serverCert')
+
+# Step 2 - Server Certificate Verification
+#wget https://raw.githubusercontent.com/alonitac/atech-devops-june-2023/main/networking_project/tls_webserver/cert-ca-aws.pem
+openssl verify -CAfile cert-ca-aws.pem <<< "$SERVER_CERT"
+echo "$SERVER_CERT" > server_cert.pem
+VERIFY_RESULT=$?
+
+if [ $VERIFY_RESULT -ne 0 ]; then
+    echo "Server Certificate is invalid."
+    exit 5
+fi
+
+# Step 3 - Generate Master Key
+openssl rand -base64 32 > master_key.txt
+# Step 4 - Encrypt Master Key and Send to Server
+ENCRYPTED_MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in master_key.txt -outform DER server_cert.pem | base64 -w 0)
+# Step 5 - Send Encrypted Master Key to Server
+ENCRYPTEDMASTER='{
+    "sessionID": "'$SESSION_ID'",
+    "masterKey": "'$ENCRYPTED_MASTER_KEY'",
+    "sampleMessage": "Hi server, please encrypt me and send to client!"
+}'
+RESPONSE=$(curl -X POST http://$PUBLIC_IP:8080/keyexchange -H "Content-Type: application/json" -d "$ENCRYPTEDMASTER")
+# Step 6 - Decrypt Sample Message
+ENCRYPTEDSAMPLEMESSAGE=$(echo "$RESPONSE" | jq -r '.encryptedSampleMessage')
+echo ENCRYPTEDSAMPLEMESSAGE | base64 -d > encSampleMsgReady.txt
+MASTER_KEY=$(<master_key.txt)
+DECRYPTED_MESSAGE=$(echo "$ENCRYPTEDSAMPLEMESSAGE" | base64 -d | openssl enc -d -aes-256-cbc -pbkdf2 -k $MASTER_KEY)
+SAMPLE_MESSAGE="Hi server, please encrypt me and send to client!"
+if [ "$DECRYPTED_MESSAGE" != "$SAMPLE_MESSAGE" ]; then
+    echo "Server symmetric encryption using the exchanged master-key has failed."
+    exit 6
+fi
+
+echo "Client-Server TLS handshake has been completed successfully"
