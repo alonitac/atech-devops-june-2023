d92bfc49f14d317791a7482e45bdf81d16f0bd50
joumana <joumana.1444@gmail.com>
2023-10-15 23:04:26 +0300

    edit problems with exit 6 handshake3

--- networking_project/tlsHandshake.sh
+++ networking_project/tlsHandshake.sh
@@ -42,7 +42,8 @@ ENCRYPTEDSAMPLEMESSAGE=$(echo "$RESPONSE" | jq -r '.encryptedSampleMessage')
 echo ENCRYPTEDSAMPLEMESSAGE | base64 -d > encSampleMsgReady.txt
 MASTER_KEY=$(<master_key.txt)
 DECRYPTED_MESSAGE=$(echo "$ENCRYPTEDSAMPLEMESSAGE" | base64 -d | openssl enc -d -aes-256-cbc -pbkdf2 -k $MASTER_KEY)
-if [ "$DECRYPTED_MESSAGE" == "Hi server, please encrypt me and send to client!" ]; then
+SAMPLE_MESSAGE="Hi server, please encrypt me and send to client!"
+if [ "$DECRYPTED_MESSAGE" == "$SAMPLE_MESSAGE" ]; then
    echo "Client-Server TLS handshake has been completed successfully"
 
 else
