a4c4e14e43f558d5bd1d8e5153cab02a84e004c3
Aviciiabzah <135011650+Aviciiabzah@users.noreply.github.com>
2023-11-21 10:17:25 +0200

    Update app.py

--- docker_project/yolo5/app.py
+++ docker_project/yolo5/app.py
@@ -24,9 +24,20 @@ def predict():
     # Receives a URL parameter representing the image to download from S3
     img_name = request.args.get('imgName')
 
-    # TODO download img_name from S3, store the local image path in original_img_path
-    #  The bucket name should be provided as an env var BUCKET_NAME.
-    original_img_path = ...
+     # Define the S3 URL
+    s3_url = f'https://abzahbucket.s3.eu-central-1.amazonaws.com/tr.jpg'
+   
+
+    # Define the local path where you want to save the downloaded image
+    local_path = 'local_directory_path'
+    os.makedirs(local_path, exist_ok=True)
+
+    # Download the image from S3 using requests
+    original_img_path = os.path.join(local_path, img_name)
+    response = requests.get(s3_url)
+
+    with open(original_img_path, 'wb') as f:
+        f.write(response.content)
 
     logger.info(f'prediction: {prediction_id}/{original_img_path}. Download img completed')
 
@@ -46,8 +57,12 @@ def predict():
     # The predicted image typically includes bounding boxes drawn around the detected objects, along with class labels and possibly confidence scores.
     predicted_img_path = Path(f'static/data/{prediction_id}/{original_img_path}')
 
-    # TODO Uploads the predicted image (predicted_img_path) to S3 (be careful not to override the original image).
+    s3_upload_url = f'https://{images_bucket}.s3.amazonaws.com/{quote(predicted_img_path.name)}'
+    with open(predicted_img_path, 'rb') as predicted_image_file:
+        response = requests.put(s3_upload_url, data=predicted_image_file)
 
+
+    
     # Parse prediction labels and create a summary
     pred_summary_path = Path(f'static/data/{prediction_id}/labels/{original_img_path.split(".")[0]}.txt')
     if pred_summary_path.exists():
@@ -72,7 +87,7 @@ def predict():
             'time': time.time()
         }
 
-        # TODO store the prediction_summary in MongoDB
+        collection.insert_one(prediction_summary)
 
         return prediction_summary
     else:
