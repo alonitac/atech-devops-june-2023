7fcbba74b931066902821db71b458b39e1aac441
alonit <alon.itach@mail.huji.ac.il>
2023-12-19 19:38:43 +0200

    fixes

--- create-aws-users.sh
+++ create-aws-users.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+PROFILE=''
+
 # Read the password from user input
-read -p "Enter password for new users: " password
+read -p "Enter password for new users: " PASSWORD
 
 # Loop through the users file and create the users with the specified password
 while read user; do
 
     echo "creating $user"
 
-    aws iam create-user --user-name "$user" --profile elv
-    aws iam add-user-to-group --group-name students --user-name "$user" --profile elv
-    aws iam create-login-profile --user-name "$user" --password "$password" --password-reset-required --profile elv
+    aws iam create-user --user-name "$user" --profile $PROFILE
+    aws iam add-user-to-group --group-name students --user-name "$user" --profile $PROFILE
+    aws iam create-login-profile --user-name "$user" --password "$PASSWORD" --password-reset-required --profile $PROFILE
     sleep 3
 done < users.txt
